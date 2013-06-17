f03fca39e6a26aa44b1dc183c88e8c68e6cf492f
László Monda <laci@monda.hu>
2013-06-17 08:17:57 +0200

    Clarify instructions regarding the API key.

--- twitter-backup.py
+++ twitter-backup.py
@@ -8,8 +8,8 @@ import oauth2 as oauth
 from StringIO import StringIO
 from urlparse import parse_qsl
 
-# Please don't use this key and secret in your script.
-# You can request your own at https://dev.twitter.com/apps/new
+# Please don't use this key and secret if you create a new version of this script.
+# You can request your own API key at https://dev.twitter.com/apps/new
 # (If you fork my repo to merely submit a pull request then you don't need to change this.)
 consumer_key = 'I5Qy02p5CrIXw8Sa9ohw'
 consumer_secret = 'ubG7dkIS6g2cjYshXM6gtN6dSZEekKTRZMKgjYIv4'
